4f880082f61af36ea44b26743d1d7141aef52871
omkarrr123 <omkarvedpathak727@gmail.com>
2025-11-03 22:56:08 +0530

    Add files via upload
    ADD ASSIGNMENT_3 SOLUTION Q2-5

---
+++ Assignment/assignment_3/assignment3.4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void DisplayConverter(char CValue)
+{
+    if()
+}
+int main()
+{
+    char cValue = '\0';
+    
+    printf("Enter character\n");
+    scanf("%c",&cValue);
+
+    DisplayConverter(cValue);
+    
+    return 0;
+}
