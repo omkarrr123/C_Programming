4249f80a12e8d4e69d2249d4d7cb58c47eb053ed
omkarrr123 <omkarvedpathak727@gmail.com>
2025-11-03 12:33:20 +0530

    Add files via upload
    ADD ASSIGNMENT_1 SOLUTION Q1-5

---
+++ Assignment/assignment_1/assignment1.4.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdbool.h>
+bool Check (int iNo)
+{
+    if((iNo%5)==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number: \n");
+    scanf("%d",&iValue);
+
+    bRet = Check(iValue);
+
+    if(bRet == true)
+    {
+        printf("Divisible by 5");
+    }
+    else
+    {
+        printf("Not divisble by 5");
+    }
+
+    return 0;
+}
